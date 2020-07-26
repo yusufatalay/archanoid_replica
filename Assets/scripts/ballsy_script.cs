51dd9436778a086b0f768291eee5a57afd9fca6b
Yusuf <yusuftahaatalay14@gmail.com>
2020-07-26 14:00:52 +0300

    main  mechanics of the game is completed

---
+++ Assets/scripts/ballsy_script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballsy_script : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D ball_rb;
+
+    [SerializeField] private float speed;
+
+    private int func_counter = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball_rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(func_counter==0) directionSelector(1);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(func_counter ==0) directionSelector(-1);
+        }
+        
+        
+        
+    }
+
+    void directionSelector(int direction)
+    {
+        if (direction == 1)
+        {
+            Vector2 dir_vec = new Vector2(1f,Mathf.Sqrt(3));
+            ball_rb.AddForce(dir_vec*speed*Time.deltaTime,ForceMode2D.Impulse);
+        }
+        else  
+        {
+            Vector2 dir_vec = new Vector2(-1f,Mathf.Sqrt(3));
+            ball_rb.AddForce(dir_vec*speed*Time.deltaTime,ForceMode2D.Impulse);
+        }
+        func_counter++;
+    }
+}
---
+++ Assets/scripts/player_movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    //[SerializeField] private Rigidbody2D player_rb;
+
+    [SerializeField] private float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  player_rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.LeftArrow)) transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
