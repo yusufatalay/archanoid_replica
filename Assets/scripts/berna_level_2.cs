f02eacebdf3509f8c0ad0cf842f33c523ddb2ca6
Yusuf <yusuftahaatalay14@gmail.com>
2020-07-26 15:30:01 +0300

    game is finished

---
+++ Assets/scripts/berna_level_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class berna_level_2 : MonoBehaviour
+{
+    [SerializeField] private int health =2;
+    [SerializeField] private SpriteRenderer sprnd;
+
+    private void Start()
+    {
+        sprnd = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (health == 2)
+        {
+            sprnd.color =Color.gray;
+            health--;
+        }
+        else if(health <2)  Destroy(gameObject);
+        
+    }
+}
--- Assets/scripts/berna_script.cs
+++ Assets/scripts/berna_script.cs
@@ -2,11 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class berna_script : MonoBehaviour
 {
+    public int bernas;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(gameObject);
+        bernas--;
+
+        //Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        Debug.Log(bernas); 
+        if (bernas == 0)
+        {
+               
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
